3225ad3aa3cccaee87846a0ccbc2494e619ed148
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-09-27 16:03:59 -0600

    Edited LoadScene to change scene when Space is pressed

--- mini_solar_sys/Assets/Scripts/LoadScene.cs
+++ mini_solar_sys/Assets/Scripts/LoadScene.cs
@@ -8,13 +8,21 @@ public class LoadScene : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene("Solar_Sys_Scene");
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ButtonLoad();
+    }
+
+    public void ButtonLoad()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Solar_Sys_Scene");
+        }
     }
 
 }
