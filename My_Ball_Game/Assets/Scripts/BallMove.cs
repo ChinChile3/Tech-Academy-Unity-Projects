c98538eb98fd4837b37ba52d3f4ccb0be59fa120
ChinChile3 <104886422+ChinChile3@users.noreply.github.com>
2022-09-27 22:04:45 -0600

    Game Assignment

---
+++ My_Ball_Game/Assets/Scripts/BallMove.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallMove : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rigBody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigBody = GetComponent<Rigidbody>();
+
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHoriz = Input.GetAxis("Horizontal");
+        float moveVert = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHoriz, 0.0f, moveVert);
+        rigBody.AddForce(movement * speed);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "GoalCylinder")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("Collision Detected");
+        }
+    }
+}
